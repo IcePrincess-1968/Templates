d20971168ad3e1b4b4db60623d70a3dd02715f70
IcePrincess_1968 <nadal295704@126.com>
2018-01-27 17:42:05 +0800

    更新 【模板】快速数论变换(NTT).cpp

--- 多项式/【模板】快速数论变换(NTT).cpp
+++ 多项式/【模板】快速数论变换(NTT).cpp
@@ -4,10 +4,11 @@ const LL MOD=998244353;
 const int G=3;
 //998244353的原根G=3
 
-//快速幂
+LL a[300048],b[300048],wn_pos[300048],wn_neg[300048],inv;
+
 LL quick_pow(LL x,LL y)
 {
-	x%=MOD;LL res=1;
+	LL res=1;x%=MOD;
 	while (y)
 	{
 		if (y&1)
@@ -21,10 +22,20 @@ LL quick_pow(LL x,LL y)
 	return res;
 }
 
+void init_wn(int len)
+{
+	int clen;
+	for (clen=2;clen<=len;clen<<=1)
+	{
+		wn_pos[clen]=quick_pow(G,(MOD-1)/clen);
+		wn_neg[clen]=quick_pow(G,(MOD-1)-(MOD-1)/clen);
+	}
+}
+
 void NTT(LL c[],int len,int fl)
 {
-	int i,j,k,clen,id=0;
-	for (i=(len>>1),j=1;j<len;j++)
+	int i,j,k,clen;
+	for (i=(len>>1),j=1;j<=len-1;j++)
 	{
 		if (i<j) swap(c[i],c[j]);
 		for (k=(len>>1);i&k;k>>=1) i^=k;
@@ -32,14 +43,13 @@ void NTT(LL c[],int len,int fl)
 	}
 	for (clen=2;clen<=len;clen<<=1)
 	{
-		LL wn=quick_pow(G,fl==1?(MOD-1)/clen:(MOD-1)-(MOD-1)/clen);
-	  	//这里容易写错！注意iDFT的时候所有的原根指数要取负！-(MOD-1)/clen不好算，利用原根的性质g^(MOD-1)=1,把指数转成正数
-    	for (j=0;j<len;j+=clen)
+		LL wn=(fl==1?wn_pos[clen]:wn_neg[clen]);
+		for (j=0;j<len;j+=clen)
 		{
 			LL w=1ll;
 			for (k=j;k<j+(clen>>1);k++)
 			{
-				LL tmp1=c[k]%MOD,tmp2=((c[k+(clen>>1)]%MOD)*w)%MOD;
+				LL tmp1=c[k]%MOD,tmp2=(c[k+(clen>>1)]%MOD*w)%MOD;
 				c[k]=(tmp1+tmp2)%MOD;c[k+(clen>>1)]=((tmp1-tmp2)%MOD+MOD)%MOD;
 				w=(w*wn)%MOD;
 			}
@@ -47,16 +57,17 @@ void NTT(LL c[],int len,int fl)
 	}
 	if (fl==-1)
 	{
-		LL inv=quick_pow(len,MOD-2);
-		for (i=0;i<len;i++) c[i]=(c[i]%MOD*inv)%MOD;
+		for (i=0;i<=len-1;i++) c[i]=(c[i]%MOD*inv)%MOD;
 	}
-}	
+}
 
 void calc_NTT()
 {
-	int i,l=(1<<18);
+	int i,l=1;
+	while (l<=n+m) l<<=1;
+	init_wn(l);inv=quick_pow(l,MOD-2);
 	NTT(a,l,1);
 	NTT(b,l,1);
-	for (i=0;i<l;i++) a[i]=(a[i]*b[i])%MOD;
+	for (i=0;i<=l-1;i++) a[i]=(a[i]*b[i])%MOD;
 	NTT(a,l,-1);
 }
