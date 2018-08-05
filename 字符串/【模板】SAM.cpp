a4e791312ccdca40a32cb08ad525267f01318cbf
IcePrincess_1968 <nadal295704@126.com>
2018-08-05 18:08:38 +0800

    update a much faster template of SA and add SAM

---
+++ 字符串/【模板】SAM.cpp
@@ -0,0 +1,25 @@
+const int MAXN=1e6+48;
+
+char s[MAXN+48];int len;
+vector<int> v[MAXN*2];int sz[MAXN*2];
+//sz数组当前记录的是SAM的这个节点是否包含一个新的后缀 
+//在parent树上对SAM做一遍深搜，统计sz的子树和，可以得到每个节点的right集合大小 
+
+namespace SAM
+{
+	int tot,nxt[MAXN*2][27],par[MAXN*2],maxn[MAXN*2],root,last;
+	inline void init() {tot=1;root=last=1;}
+	inline void insert(char ch)
+	{
+		int p=last,np=++tot;;maxn[np]=maxn[p]+1;sz[np]=1;int w=ch-'a'+1;
+		while (p && !nxt[p][w]) nxt[p][w]=np,p=par[p];
+		if (!p) {par[np]=root;last=np;return;}
+		int q=nxt[p][w];
+		if (maxn[q]==maxn[p]+1) {par[np]=q;last=np;return;}
+		int nq=++tot;maxn[nq]=maxn[p]+1;
+		memcpy(nxt[nq],nxt[q],sizeof(nxt[q]));
+		par[nq]=par[q];par[np]=nq;par[q]=nq;
+		while (p && nxt[p][w]==q) nxt[p][w]=nq,p=par[p];
+		last=np;
+	}
+}
--- 字符串/【模板】后缀数组+高度数组.cpp
+++ 字符串/【模板】后缀数组+高度数组.cpp
@@ -1,49 +1,90 @@
-//O(n*logn*logn) 
-int rank[1000048],tmp[1000048],sa[1000048],lcp[1000048];
-char s[1000048];
-int len,k;
+const int MAXN=1e5;
 
-bool compare_sa(int x,int y)
+char s[MAXN+48];
+int sa[MAXN+48],rnk[MAXN+48],tmp[MAXN+48],lcp[MAXN+48],k,len;
+
+struct List
 {
-	if (rank[x]!=rank[y]) return rank[x]<rank[y];
-	int tmp1=x+k<=len?rank[x+k]:-1,tmp2=y+k<=len?rank[y+k]:-1;
+	int head[MAXN+48],nxt[MAXN+48],cur[MAXN+48],tot;Pair val[MAXN+48];
+	inline void init() {memset(head,0,sizeof(head));tot=0;}
+	inline void insert(int pos,Pair v)
+	{
+		if (!head[pos]) {head[pos]=++tot;cur[pos]=head[pos];val[tot]=v;nxt[tot]=0;return;}
+		nxt[cur[pos]]=++tot;cur[pos]=tot;
+		val[tot]=v;nxt[tot]=0;
+	}
+}l1,l2;
+
+inline bool compare_sa(int x,int y)
+{
+	if (rnk[x]!=rnk[y]) return rnk[x]<rnk[y];
+	int tmp1=x+k<=len?rnk[x+k]:-1,tmp2=y+k<=len?rnk[y+k]:-1;
 	return tmp1<tmp2;
 }
 
-void construct_sa()
+inline int getrnk(int x,bool type)
+{
+	if (!type) return rnk[x];
+	return x+k<=len?rnk[x+k]:0;
+}
+
+bool isfirst;int maxind;
+
+inline void Sort()
+{
+	int i,j;
+	l1.init();
+	for (i=1;i<=len;i++) l1.insert(getrnk(sa[i],true),mp(getrnk(sa[i],false),sa[i]));
+	l2.init();
+	if (isfirst)
+	{
+		for (i=97;i<=122;i++)
+			for (j=l1.head[i];j;j=l1.nxt[j])
+				l2.insert(l1.val[j].x,mp(l1.val[j].y,0));
+		int tt=0;
+		for (i=97;i<=122;i++)
+			for (j=l2.head[i];j;j=l2.nxt[j])
+				sa[++tt]=l2.val[j].x;
+		return;
+	}
+	for (i=0;i<=maxind;i++)
+		for (j=l1.head[i];j;j=l1.nxt[j])
+			l2.insert(l1.val[j].x,mp(l1.val[j].y,0));
+	int tt=0;
+	for (i=0;i<=maxind;i++)
+		for (j=l2.head[i];j;j=l2.nxt[j])
+			sa[++tt]=l2.val[j].x;
+}
+
+inline void construct_sa()
 {
 	int i;
-	for (i=1;i<=len;i++)
+	for (i=1;i<=len;i++) sa[i]=i,rnk[i]=s[i];
+	for (k=0;k<=len;k==0?k++:k<<=1)
 	{
-		sa[i]=i;
-		rank[i]=s[i];
-	}
-	int cnt;
-	for (k=0;k<=len;k==0?k++:k*=2)
-	{
-		sort(sa+1,sa+len+1,compare_sa);
-		tmp[sa[1]]=1;cnt=0;
+		if (k==0) isfirst=true; else isfirst=false;
+		Sort();
+		tmp[sa[1]]=1;int cnt=0;
 		for (i=2;i<=len;i++)
 		{
 			tmp[sa[i]]=tmp[sa[i-1]]+(compare_sa(sa[i-1],sa[i])?1:0);
 			cnt+=(tmp[sa[i]]!=tmp[sa[i-1]]);
 		}
-		for (i=1;i<=len;i++) rank[i]=tmp[i];
-		if (cnt>=len-1) break;//若已经完全有序则直接退出，重要优化 
+		maxind=tmp[sa[len]];
+		for (i=1;i<=len;i++) rnk[i]=tmp[i];
+		if (cnt>=len-1) break;
 	}
 }
 
-//rank[i]表示原串中第i个字符开始的后缀在sa数组中的位置 
-//lcp[i]记录sa[i]和sa[i+1]起始的后缀的最长公共前缀 
-void construct_lcp()
+inline void construct_lcp()
 {
 	int i,j,h=0;
-	for (i=1;i<=len;i++) rank[sa[i]]=i;
+	for (i=1;i<=len;i++) rnk[sa[i]]=i;
 	for (i=1;i<=len;i++)
 	{
-		j=sa[rank[i]-1];
+		j=sa[rnk[i]-1];
 		if (h) h--;
 		while (i+h<=len && j+h<=len && s[i+h]==s[j+h]) h++;
-		lcp[rank[i]-1]=h;
+		lcp[rnk[i]-1]=h;
 	}
 }
